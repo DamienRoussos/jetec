8196465c40aea3a8f2306f7dc3feaa85b5855a36
DamienRoussos <61167472+DamienRoussos@users.noreply.github.com>
2024-04-16 18:55:43 +0200

    added a user selector for the postcode, using the provided postcodes

--- index.js
+++ index.js
@@ -1,38 +1,60 @@
-const readline = require('node:readline');
-const axios = require('axios').default;
+// const inquirer = require('inquirer');
+// const axios = require('axios').default;
+// const inquirer = require('')
+import axios from 'axios';
+import { select } from '@inquirer/prompts';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const postcodes = ["CT12EH", "BS14DJ", "L40TH", "NE97TY", "SW1A1AA", "CF118AZ", "M160RA", "EH11RE", "BN11AE", "CB74DL", "LS27HY", "G38AG", "PL40DW", "B263QJ", "DH45QZ", "BT71NN"]
+const postcodes = [
+  'CT12EH',
+  'BS14DJ',
+  'L40TH',
+  'NE97TY',
+  'SW1A1AA',
+  'CF118AZ',
+  'M160RA',
+  'EH11RE',
+  'BN11AE',
+  'CB74DL',
+  'LS27HY',
+  'G38AG',
+  'PL40DW',
+  'B263QJ',
+  'DH45QZ',
+  'BT71NN',
+];
 
 async function apicall(postcode) {
   try {
     const response = await axios.get(
       `https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/${postcode}`
     );
-    const firstTenRestaurants = response.data.restaurants.slice(0, 10);
-    const firstTenRestaurantsDetails = firstTenRestaurants.map((restaurant) => {
-      const name = restaurant.name;
-      const cuisines = restaurant.cuisines.map((cuisine) => cuisine.name).join();
-      const rating = restaurant.rating.starRating;
-      const address = `${restaurant.address.firstLine} ${restaurant.address.postalCode} ${restaurant.address.city}`;
-      return { name, cuisines, rating, address };
-    });
+    if(response.status === 200) {
+      const firstTenRestaurants = response.data.restaurants.slice(0, 10);
+      const firstTenRestaurantsDetails = firstTenRestaurants.map((restaurant) => {
+        const name = restaurant.name;
+        const cuisines = restaurant.cuisines
+          .map((cuisine) => cuisine.name)
+          .join();
+        const rating = restaurant.rating.starRating;
+        const address = `${restaurant.address.firstLine} ${restaurant.address.postalCode} ${restaurant.address.city}`;
+        return {name, cuisines, rating, address};
+      });
 
-    console.table(firstTenRestaurantsDetails);
-
+      console.table(firstTenRestaurantsDetails);
+    } else {
+      console.log(`No restaurants found for the postcode ${postcode}`)
+    }
   } catch (error) {
-    console.log(error);
+    console.log(`Something went wrong: ${error.message}`);
   }
 }
 
-
-postcodes.forEach(postcode => console.log(postcode))
-rl.question("Please select a postcode from the list above\n", postcode => {
-  apicall(postcode)
-  rl.close()
+const userInput = await select({
+  message: "Please select a postcode",
+  choices: postcodes.map(code => {
+    return {value: code}
+  }),
+  loop: false
 })
-// apicall();
+
+apicall(userInput)
