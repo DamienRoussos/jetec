773306a26980a85c63f419055748331d2d2aa3ee
DamienRoussos <61167472+DamienRoussos@users.noreply.github.com>
2024-04-16 17:04:50 +0200

    first check on api data

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # Documentation
 
-## Getting started
-
 1. **Clone the repository**:
 
-2. **Running**:  
-(Requires Node.js installed)
+```
+git clone https://github.com/DamienRoussos/jetec
+```
+
+2. **Running**:  (Requires Node.js installed)
+
+Change directory
+```
+cd jetec
+```
+
+Install dependencies
+```
+npm install
+```
+
+Run the project
+```
+npm run dev
+```
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("running")
+const axios = require('axios').default;
+
+async function apicall() {
+  try {
+    const response = await axios.get(
+      'https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/CT12EH'
+    );
+    const firstTenRestaurants = response.data.restaurants.slice(0, 10);
+    const firstTenRestaurantsDetails = firstTenRestaurants.map((restaurant) => {
+      const name = restaurant.name;
+      const cuisines = restaurant.cuisines.map((cuisine) => cuisine.name);
+      const rating = restaurant.rating.starRating;
+      const address = `${restaurant.address.firstLine} ${restaurant.address.postalCode} ${restaurant.address.city}`;
+      return { name, cuisines, rating, address };
+    });
+
+    console.table(firstTenRestaurantsDetails);
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apicall();
